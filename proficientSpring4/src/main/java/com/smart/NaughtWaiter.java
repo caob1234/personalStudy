28f7b0c02e2a9c7378c4ba682f4f8730df3d69b4
grassmonkey <q799229025@163.com>
2019-10-10 21:25:54 +0800

    test @annotation

--- proficientSpring4/src/main/java/com/smart/NaughtWaiter.java
+++ proficientSpring4/src/main/java/com/smart/NaughtWaiter.java
@@ -1,4 +1,16 @@
 package com.smart;
 
-public class NaughtWaiter {
+import com.smart.aspectj.anno.NeedTest;
+
+public class NaughtWaiter implements Waiter {
+    @Override
+    @NeedTest
+    public void greetTo(String name) {
+        System.out.println("NaughtWaiter: greet to"+name+"...");
+    }
+
+    @Override
+    public void serveTo(String name) {
+        System.out.println("NaughtWaiter: serve to"+name+"...");
+    }
 }
--- proficientSpring4/src/main/java/com/smart/aspectj/fun/PointcutFunTest.java
+++ proficientSpring4/src/main/java/com/smart/aspectj/fun/PointcutFunTest.java
@@ -1,4 +1,18 @@
 package com.smart.aspectj.fun;
 
+import com.smart.Waiter;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.testng.annotations.Test;
+
 public class PointcutFunTest {
+    @Test
+    public void pointcut(){
+        String configpath="com/smart/aspectj/fun/beans.xml";
+        ApplicationContext ctx = new ClassPathXmlApplicationContext(configpath);
+        Waiter naiveWaiter = (Waiter) ctx.getBean("naiveWaiter");
+        naiveWaiter.greetTo("Tom");
+        Waiter naughtWaiter= (Waiter) ctx.getBean("naughtWaiter");
+        naughtWaiter.greetTo("John");
+    }
 }
