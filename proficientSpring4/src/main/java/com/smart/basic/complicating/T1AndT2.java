2b3545a52cde6fe4867148a8a8c34525e97cedac
grassmonkey <q799229025@163.com>
2020-02-22 10:19:32 +0800

    practise notifyAll and notify

---
+++ proficientSpring4/src/main/java/com/smart/basic/complicating/T1AndT2.java
@@ -0,0 +1,14 @@
+package com.smart.basic.complicating;
+
+public class T1AndT2 {
+//    synchronized(sharedMonitor){
+//        <setup condition for T2>
+//                sharedMonitor.notify();
+//    }
+//    while(someCondition){
+//        //Point 1
+//        synchronized (sharedMonitor){
+//            sharedMonitor.wait();
+//        }
+//    }
+}
