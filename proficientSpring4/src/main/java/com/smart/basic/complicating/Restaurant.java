eaf7308600e85699f9eb18c61d368b9d1926ed99
grassmonkey <q799229025@163.com>
2020-02-25 21:49:18 +0800

    update Restaurant for BusBoy

--- proficientSpring4/src/main/java/com/smart/basic/complicating/Restaurant.java
+++ proficientSpring4/src/main/java/com/smart/basic/complicating/Restaurant.java
@@ -18,6 +18,13 @@ class Meal{
         return "Meal{" + "orderNum=" + orderNum + '}';
     }
 }
+class TableWare{
+    private Meal meal;
+
+    public TableWare(Meal meal) {
+        this.meal = meal;
+    }
+}
 class WaitPerson implements Runnable{
     private Restaurant restaurant;
 
@@ -32,15 +39,14 @@ class WaitPerson implements Runnable{
                 synchronized (this) {
                     while (restaurant.meal == null) {
                         wait();
-                        print("After waitPerson wait");
                     }
                 }
                 print("Waitperson got "+restaurant.meal);
                 TimeUnit.MILLISECONDS.sleep(100);
-                synchronized (restaurant.chef){
-                    print("After restaurant.chef sync");
+                synchronized (restaurant.busboy){
                     restaurant.meal=null;
-                    restaurant.chef.notifyAll();
+                    print("Customers have finished the eating.Notify busboy!");
+                    restaurant.busboy.notifyAll();
                 }
             }
         } catch (InterruptedException e) {
@@ -59,23 +65,23 @@ class Chef implements Runnable{
         try {
             while (!Thread.interrupted()){
                 synchronized (this){
-                    while (restaurant.meal!=null){
-                        print("Before chef wait");
+                    while (restaurant.meal!=null||restaurant.tableWare!=null){
                         wait();
-                        print("After chef wait");
                     }
                 }
                 if (++count==10){
                     print("Out of food.closing");
                     restaurant.executorService.shutdownNow();
+                    TimeUnit.MILLISECONDS.sleep(100);
+//                    return;
                 }
                 print("Order up! ");
                 synchronized (restaurant.waitPerson){
                     restaurant.meal=new Meal(count);
+                    restaurant.tableWare=new TableWare(restaurant.meal);
+                    print("The Chef finished a dish.Notify waitPerson!");
                     restaurant.waitPerson.notifyAll();
-                    print("After waitPerson notifyAll");
                 }
-                print("Ready sleep or wait");
                 TimeUnit.MILLISECONDS.sleep(100);
             }
         }catch (InterruptedException e){
@@ -83,15 +89,45 @@ class Chef implements Runnable{
         }
     }
 }
+class Busboy implements Runnable{
+    private Restaurant restaurant;
+
+    public Busboy(Restaurant restaurant) {
+        this.restaurant = restaurant;
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!Thread.interrupted()){
+                synchronized (this){
+                    while (restaurant.tableWare==null){
+                        wait();
+                    }
+                }
+
+                synchronized (restaurant.chef){
+                    restaurant.tableWare=null;
+                    print("Busboy washed the dishes.Notify chef!");
+                    restaurant.chef.notifyAll();
+                }
+            }
+        }catch (InterruptedException e){
+            print("BusBoy interrupted");
+        }
+    }
+}
 public class Restaurant {
     Meal meal;
+    TableWare tableWare;
     ExecutorService executorService= Executors.newCachedThreadPool();
     WaitPerson waitPerson=new WaitPerson(this);
     Chef chef=new Chef(this);
-
+    Busboy busboy=new Busboy(this);
     public Restaurant() {
         executorService.execute(chef);
         executorService.execute(waitPerson);
+        executorService.execute(busboy);
     }
     public static void main(String[] args){
         new Restaurant();
