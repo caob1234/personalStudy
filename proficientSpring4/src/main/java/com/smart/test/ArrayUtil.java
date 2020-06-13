1c50dc5d7bf574634a9d944f56aac8e68cb1279d
grassmonkey <q799229025@163.com>
2020-06-13 09:43:54 +0800

    finished all

--- proficientSpring4/src/main/java/com/smart/test/ArrayUtil.java
+++ proficientSpring4/src/main/java/com/smart/test/ArrayUtil.java
@@ -1,10 +1,16 @@
 package com.smart.test;
 
-import java.util.ArrayList;
-
 public class ArrayUtil {
 
-    public int findStartIndex(int[] dist, int[] search) throws Exception{
+    /**
+     * 第一题方法
+     *
+     * @param dist
+     * @param search
+     * @return
+     * @throws Exception
+     */
+    public int findStartIndex(int[] dist, int[] search) throws Exception {
         int index = -1;
         int length = 0;
         for (int j = 0; j < dist.length; j++) {
@@ -12,17 +18,17 @@ public class ArrayUtil {
                 for (int i = 0; i < search.length; i++) {
                     if ((j + i) < dist.length && search[i] == dist[j + i]) {
                         length++;
-                    }else {
+                    } else {
                         break;
                     }
                 }
                 if (length == search.length) {
                     index = j;
-                    System.out.println("start point:" + j);
+//                    System.out.println("start point:" + j);
                     break;
                 } else {
-                    System.out.println("--------:" + "-1");
-                    length=0;
+//                    System.out.println("--------:" + "-1");
+                    length = 0;
                     continue;
                 }
 
@@ -32,55 +38,40 @@ public class ArrayUtil {
         }
         return index;
     }
-    public int indexOf(int[] search,int[] dist) throws Exception{
-        int number=0;
-        int index = -1;
-        int length = 1;
-        for (int j = 0; j < dist.length; j++) {
-            if (search[0] == dist[j]) {
-                for (int i = 1; i < search.length; i++) {
-                    int distIndex=findIndexByValue(search[i],j+i,dist);
-                    if (distIndex !=-1) {
-                        length++;
-                        j=distIndex;
-                    }else {
-                        break;
-                    }
-                }
-                if (length == search.length) {
-                    index = j;
-                    System.out.println("start point:" + j);
-                    number++;
-                    break;
-                } else {
-                    System.out.println("--------:" + "-1");
-                    length=0;
-                    continue;
-                }
 
-            } else {
-                continue;
-            }
-        }
-        return number;
+    /**
+     * 第二题方法
+     *
+     * @param search
+     * @param dist
+     * @return
+     * @throws Exception
+     */
+    public int indexOf(int[] search, int[] dist) throws Exception {
+        int rootIndex = this.findIndexByValue(search[0], 0, dist);
+        ChildNode childNode = new ChildNode(rootIndex);
+        this.visitTree(rootIndex, search[1], search, dist, childNode);
+        return this.number;
     }
-    public int findIndexByValue(int value,int startIndex,int[] dist){
-        int index=-1;
-        if (startIndex==dist.length||startIndex>dist.length){
+
+    public int findIndexByValue(int value, int startIndex, int[] dist) {
+        int index = -1;
+        if (startIndex == dist.length || startIndex > dist.length) {
             return index;
         }
-        for (int i = startIndex+1; i < dist.length; i++) {
-            if (value==dist[i]){
-                index=i;
+        for (int i = startIndex + 1; i < dist.length; i++) {
+            if (value == dist[i]) {
+                index = i;
                 break;
             }
         }
         return index;
     }
-    public int NumberOfValue(int startIndex,int lastIndex,int value,int[] dist){
-        int number=0;
-        for (int i = startIndex; i < lastIndex+1; i++) {
-            if (value==dist[i]){
+
+    public int NumberOfValue(int startIndex, int lastIndex, int value, int[] dist) {
+        int number = 0;
+        for (int i = startIndex; i < lastIndex + 1; i++) {
+            if (value == dist[i]) {
                 number++;
             }
         }
@@ -89,49 +80,49 @@ public class ArrayUtil {
 
     /**
      * Recursively traverse the child nodes of tree
-     * @param rootIndex root node index of dist array
+     *
+     * @param rootIndex   root node index of dist array
      * @param searchValue A value of search array
-     * @param search search array
-     * @param dist dist array
-     * @param childNode Tree node for recursively traverse
+     * @param search      search array
+     * @param dist        dist array
+     * @param childNode   Tree node for recursively traverse
      */
-    public void visitTree(int rootIndex,int searchValue,int[] search,int[] dist,ChildNode childNode){
-        int nextIndex=findIndexByValue(searchValue,rootIndex,dist);
-        while (nextIndex!=-1){
-            ChildNode nextChildNode=new ChildNode(nextIndex);
-            int nextValue=getNextValue(searchValue,search);
+    public void visitTree(int rootIndex, int searchValue, int[] search, int[] dist, ChildNode childNode) {
+        int nextIndex = findIndexByValue(searchValue, rootIndex, dist);
+        while (nextIndex != -1) {
+            ChildNode nextChildNode = new ChildNode(nextIndex);
+            int nextValue = getNextValue(searchValue, search);
             childNode.childNodes.add(nextChildNode);
-            if (nextValue!=-1){
-                visitTree(nextIndex,nextValue,search,dist,nextChildNode);
-
-                nextIndex=findIndexByValue(dist[nextChildNode.data],nextChildNode.data,dist);
-                //code
-
-            }else {
+            if (nextValue != -1) {
+                visitTree(nextIndex, nextValue, search, dist, nextChildNode);
+                nextIndex = findIndexByValue(dist[nextChildNode.data], nextChildNode.data, dist);
+            } else {
                 number++;
-                int index=findIndexByValue(searchValue,nextChildNode.data,dist);
-                while (index!=-1){
+                int index = findIndexByValue(searchValue, nextChildNode.data, dist);
+                while (index != -1) {
                     number++;
                     childNode.childNodes.add(new ChildNode(index));
-                    index=findIndexByValue(searchValue,index,dist);
+                    index = findIndexByValue(searchValue, index, dist);
                 }
                 break;
             }
         }
     }
-    public int number=0;
+
+    public int number = 0;
 
     /**
      * Get next value of search array via searchValue
+     *
      * @param searchValue
      * @param search
      * @return
      */
-    private int getNextValue(int searchValue,int[] search) {
-        int nextValue=-1;
+    private int getNextValue(int searchValue, int[] search) {
+        int nextValue = -1;
         for (int i = 0; i < search.length; i++) {
-            if (searchValue==search[i]&&(i+1)<search.length){
-                nextValue=search[i+1];
+            if (searchValue == search[i] && (i + 1) < search.length) {
+                nextValue = search[i + 1];
                 break;
             }
         }
--- proficientSpring4/src/test/java/com/smart/test/ArrayUtilTest.java
+++ proficientSpring4/src/test/java/com/smart/test/ArrayUtilTest.java
@@ -9,32 +9,27 @@ public class ArrayUtilTest {
 
     private ArrayUtil arrayUtil = new ArrayUtil();
 
-//    @Test
-//    public void testFindStartIndex() throws Exception {
-//        int[] dist = {9, 3, 3, 5, 4, 6, 4, 3, 6, 5,4,6};
-//        int[] search = {5, 4, 6};
-//        System.out.println(arrayUtil.findStartIndex(dist,search));
-//        int[] dist1 = {9, 3, 3, 5, 5, 6, 4, 3, 6, 5,4,6};
-//        int[] search1 = {5, 4, 6};
-//        System.out.println(arrayUtil.findStartIndex(dist1,search1));
-//        int[] dist2 = {1,5,2,3,4,5,6};
-//        int[] search2 = {5, 4, 6};
-//        System.out.println(arrayUtil.findStartIndex(dist2,search2));
-//        int[] dist3 = {5,4,6,2,3,4,5,6};
-//        int[] search3 = {5, 4, 6};
-//        System.out.println(arrayUtil.findStartIndex(dist3,search3));
-//    }
+    @Test
+    public void testFindStartIndex() throws Exception {
+        int[] dist = {9, 3, 3, 5, 4, 6, 4, 3, 6, 5,4,6};
+        int[] search = {5, 4, 6};
+        System.out.println(arrayUtil.findStartIndex(dist,search));
+        int[] dist1 = {9, 3, 3, 5, 5, 6, 4, 3, 6, 5,4,6};
+        int[] search1 = {5, 4, 6};
+        System.out.println(arrayUtil.findStartIndex(dist1,search1));
+        int[] dist2 = {1,5,2,3,4,5,6};
+        int[] search2 = {5, 4, 6};
+        System.out.println(arrayUtil.findStartIndex(dist2,search2));
+        int[] dist3 = {5,4,6,2,3,4,5,6};
+        int[] search3 = {5, 4, 6};
+        System.out.println(arrayUtil.findStartIndex(dist3,search3));
+    }
 
     @Test
     public void testIndexOf() throws Exception {
-        int[] dist = {1,5,5,2,4,4,3,6,4,7,6,3,6};
-        int[] search = {5, 2};
+        int[] dist = {1,5,5,2,4,4,3,6,4,7,6};
+        int[] search = {5,4,6};
 
-
-        int rootIndex=arrayUtil.findIndexByValue(search[0],0,dist);
-        ChildNode childNode=new ChildNode(rootIndex);
-        arrayUtil.visitTree(rootIndex,search[1],search,dist,childNode);
-        System.out.println(arrayUtil.number);
         System.out.println(arrayUtil.indexOf(search,dist));
 
     }
