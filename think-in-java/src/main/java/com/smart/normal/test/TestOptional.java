78154f3d12450a74be057b8c9ffcbf74fb431f5a
grassmonkey <q799229025@163.com>
2020-10-26 20:50:47 +0800

    add test Optional

---
+++ think-in-java/src/main/java/com/smart/normal/test/TestOptional.java
@@ -0,0 +1,22 @@
+
+package com.smart.normal.test;
+
+import java.util.Optional;
+
+/**
+ * <p>
+ *
+ * @author caob
+ * @version 1.0, 2020/10/26
+ */
+public class TestOptional {
+    public static void main(String[] args){
+        String s="Hello world!";
+        String s1=null;
+        Optional optionalS= Optional.of(s);
+        Optional optionalS1=Optional.ofNullable(s1);
+        System.out.println(optionalS.get());
+        System.out.println(optionalS1.orElse("This is null"));
+        System.out.println(optionalS.isPresent()+"|"+optionalS1.isPresent());
+    }
+}
